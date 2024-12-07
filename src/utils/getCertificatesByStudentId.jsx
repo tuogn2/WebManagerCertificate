27d01abafa03a312b11f897b94f99fe73e3177a6
tuogn2 <tranphu.chituong.9d4@gmail.com>
2024-12-08 03:17:01 +0700

    fix bug show certificate

--- src/utils/getCertificatesByStudentId.jsx
+++ src/utils/getCertificatesByStudentId.jsx
@@ -1,23 +1,39 @@
-import { initContract } from '../contract/contract';
+// import { initContract } from '../contract/contract';
 
+// const getCertificatesByStudentId = async (studentId) => {
+//   try {
+//     const { web3, contract } = await initContract();
+
+//     if (!web3) {
+//       throw new Error('Không thể khởi tạo Web3.');
+//     }
+
+//     // Gọi hàm getCertificatesByStudentId với studentId
+//     const certificates = await contract.methods
+//       .getCertificatesByStudentId(studentId)
+//       .call();
+
+//     return { success: true, data: certificates };
+//   } catch (error) {
+//     console.error('Lỗi khi truy xuất chứng chỉ:', error.message);
+//     return { success: false, message: error.message };
+//   }
+// };
+
+// export default getCertificatesByStudentId;
+
+
+import { contract } from '../contract/contractWIthinfura';
 const getCertificatesByStudentId = async (studentId) => {
   try {
-    const { web3, contract } = await initContract();
+    // Gọi hàm getCertificatesByStudentId từ hợp đồng thông minh
+    const certificates = await contract.methods.getCertificatesByStudentId(studentId).call();
 
-    if (!web3) {
-      throw new Error('Không thể khởi tạo Web3.');
-    }
-
-    // Gọi hàm getCertificatesByStudentId với studentId
-    const certificates = await contract.methods
-      .getCertificatesByStudentId(studentId)
-      .call();
-
+    console.log('Dữ liệu chứng chỉ:', certificates);
     return { success: true, data: certificates };
   } catch (error) {
     console.error('Lỗi khi truy xuất chứng chỉ:', error.message);
     return { success: false, message: error.message };
   }
 };
-
 export default getCertificatesByStudentId;
